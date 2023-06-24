5f61198e845e06d0d9f5912049aa5b35b0ceecce
WastaTariq <wastatar30@gmail.com>
2023-06-24 19:06:16 +0200

    BasicNavigation

--- src/test/java/com/cydeo/BaasicNavigation.java
+++ src/test/java/com/cydeo/BaasicNavigation.java
@@ -1,9 +1,36 @@
 package com.cydeo;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class BaasicNavigation {
+    public static void main(String[] args) {
+
+        //setup chrome and create Webdriver instance
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
 
 
-    //setup chrome and create Webdriver instance
+        // navigate.to(url)
+        driver.navigate().to("https://www.google.com");
+
+        //navigate.back()
+        driver.navigate().back();
+
+        // navigate.forward()
+        driver.navigate().forward();
+
+
+        //navigate.refresh
+        driver.navigate().refresh();
+
+
+
+
+    }
+
+
 
 
 
